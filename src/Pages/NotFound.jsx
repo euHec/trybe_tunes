398c08273893046ecc6ebe6ded58bb12f0bc001e
euHec <hectorsouzass@gmail.com>
2023-03-13 19:30:14 -0300

    Implementa pages e rotas necessãrias para a aplicação

---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export default class NofFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <span>NofFound</span>
+      </div>
+    );
+  }
+}
