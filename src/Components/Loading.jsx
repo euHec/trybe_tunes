945f9d3b550954f63c5b73eb1fbd7eff1d120db5
euHec <hectorsouzass@gmail.com>
2023-03-14 18:56:49 -0300

    Implementa formulário para identificação

---
+++ src/Components/Loading.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export default class Loading extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Carregando...</h1>
+      </div>
+    );
+  }
+}
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,77 @@
 import { Component } from 'react';
+import Proptypes from 'prop-types';
+import Loading from '../Components/Loading';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      disabled: true,
+      login: false,
+    };
+    this.handlechanges = this.handlechanges.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handlechanges = (event) => {
+    const { target } = event;
+    const { value } = target;
+
+    console.log(value);
+    const MIN_LENGTH = 3;
+    if (target.value.length >= MIN_LENGTH) {
+      this.setState({
+        disabled: false,
+        name: value,
+      });
+    } else {
+      this.setState({
+        disabled: true,
+        name: value,
+      });
+    }
+  };
+
+  handleSubmit = async () => {
+    const { history } = this.props;
+    const { name } = this.state;
+    this.setState({ login: true });
+    await createUser({ name: `${name}` });
+    history.push('/search');
+  };
+
   render() {
+    const { disabled, login } = this.state;
+    if (login) {
+      return (
+        <Loading />
+      );
+    }
     return (
       <div data-testid="page-login">
         <span>Login</span>
+        <input
+          data-testid="login-name-input"
+          onChange={ this.handlechanges }
+          placeholder="Qual o seu nome?"
+          type="text"
+        />
+        <button
+          data-testid="login-submit-button"
+          disabled={ disabled }
+          onClick={ () => this.handleSubmit() }
+        >
+          Enviar
+        </button>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: Proptypes.shape({
+    push: Proptypes.func,
+  }).isRequired,
+};
