7c35b7ff23ad97aa7675d1275d55e39e2f5621e8
euHec <hectorsouzass@gmail.com>
2023-03-15 20:22:55 -0300

    Implementa form de pesquisa na página de busca

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -2,11 +2,43 @@ import { Component } from 'react';
 import Header from '../Components/Header';
 
 export default class Search extends Component {
+  state = {
+    disabled: true,
+  };
+
+  handlechanges = ({ target }) => {
+    const MIN_LENGTH = 2;
+    const { value } = target;
+
+    if (value.length >= MIN_LENGTH) {
+      this.setState({
+        disabled: false,
+      });
+    } else {
+      this.setState({
+        disabled: true,
+      });
+    }
+  };
+
   render() {
+    const { disabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <span>Search</span>
+        <form action="">
+          <input
+            data-testid="search-artist-input"
+            onChange={ this.handlechanges }
+            type="text"
+          />
+          <button
+            disabled={ disabled }
+            data-testid="search-artist-button"
+          >
+            Procurar
+          </button>
+        </form>
       </div>
     );
   }
